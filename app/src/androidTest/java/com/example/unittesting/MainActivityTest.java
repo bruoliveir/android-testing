8da1cc05ec4d67bffe744f0815a684168fa32796
Bruno Oliveira <bru.oliveir@gmail.com>
2017-01-31 15:53:18 -0200

    Added test to check message after inserting a name

--- app/src/androidTest/java/com/example/unittesting/MainActivityTest.java
+++ app/src/androidTest/java/com/example/unittesting/MainActivityTest.java
@@ -1,5 +1,10 @@
 package com.example.unittesting;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.widget.TextView;
@@ -22,5 +27,18 @@ public class MainActivityTest {
         TextView textView = (TextView) mainActivity.findViewById(R.id.main_textview);
         String text = textView.getText().toString();
         Assert.assertEquals(mainActivity.getString(R.string.main_textview_text), text);
+    }
+
+    @Test
+    public void insertNameAndCheckMessage() {
+        final String testName = "Bob";
+        final String testMessage = "Hello, Bob!";
+        
+        Espresso.onView(ViewMatchers.withId(R.id.main_edittext))
+                .perform(ViewActions.typeText(testName), ViewActions.closeSoftKeyboard());
+        Espresso.onView(ViewMatchers.withId(R.id.main_button))
+                .perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.main_textview))
+                .check(ViewAssertions.matches(ViewMatchers.withText(testMessage)));
     }
 }
