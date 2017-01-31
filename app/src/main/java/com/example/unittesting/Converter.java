e06d276d978d8285f7d3ab16c1d51c6652994693
Bruno Oliveira <bru.oliveir@gmail.com>
2017-01-31 16:44:32 -0200

    Converter class

---
+++ app/src/main/java/com/example/unittesting/Converter.java
@@ -0,0 +1,12 @@
+package com.example.unittesting;
+
+public class Converter {
+
+    public static double mphToKmh (double mph) {
+        return 1.60934 * mph;
+    }
+
+    public static double kmhToMph (double kmh) {
+        return 0.621371 * kmh;
+    }
+}
