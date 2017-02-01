ea0eec063d1adcbc08bed911f3d804e4b99cfc15
Bruno Oliveira <bru.oliveir@gmail.com>
2017-02-01 15:19:13 -0200

    Statically import Espresso methods

--- app/src/androidTest/java/com/example/unittesting/MainActivityTest.java
+++ app/src/androidTest/java/com/example/unittesting/MainActivityTest.java
@@ -1,9 +1,5 @@
 package com.example.unittesting;
 
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.action.ViewActions;
-import android.support.test.espresso.assertion.ViewAssertions;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.widget.TextView;
@@ -13,6 +9,14 @@ import junit.framework.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -34,12 +38,11 @@ public class MainActivityTest {
 
         MainActivity mainActivity = mMainActivity.getActivity();
 
-        Espresso.onView(ViewMatchers.withId(R.id.main_edittext))
-                .perform(ViewActions.typeText(testName), ViewActions.closeSoftKeyboard());
-        Espresso.onView(ViewMatchers.withId(R.id.main_button))
-                .perform(ViewActions.click());
-        Espresso.onView(ViewMatchers.withId(R.id.main_textview))
-                .check(ViewAssertions.matches(ViewMatchers.withText(
-                        mainActivity.getString(R.string.main_textview_text, testName))));
+        onView(withId(R.id.main_edittext))
+                .perform(typeText(testName), closeSoftKeyboard());
+        onView(withId(R.id.main_button))
+                .perform(click());
+        onView(withId(R.id.main_textview))
+                .check(matches(withText(mainActivity.getString(R.string.main_textview_text, testName))));
     }
 }
